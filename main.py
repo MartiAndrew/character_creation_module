a8017bb4120c7432f74984eddcb0ec739953bffc
MartiAndrew <andrew.mishkov@gmail.com>
2022-11-03 01:28:15 +0300

    add modul2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
+
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}'
@@ -87,4 +88,5 @@ if __name__ == '__main__':
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
+
 
